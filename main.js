2498ca2b2513e8b914f2fa4c34196946ff0f469f
Ryan Macdonald <hi@ryanmacdonald.ca>
2023-05-06 18:32:14 -0400

    Update with initial design

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
 console.log('I am your elder');
+
+/** @type NodeListOf<HTMlAnchorElement> */
+const bookmarkElements = document.querySelectorAll('a[data-bookmark]');
+
+bookmarkElements.forEach(element => {
+  element.addEventListener('click', event => {
+    event.preventDefault();
+
+    if (window.sidebar && window.sidebar.addPanel) {
+      // Mozilla Firefox Bookmark
+      window.sidebar.addPanel(document.title, window.location.href, '');
+    } else if (window.external && 'AddFavorite' in window.external) {
+      // IE Favorite
+      window.external.AddFavorite(location.href, document.title);
+    } else if (window.opera && window.print) {
+      // Opera Hotlist
+      this.title = document.title;
+      return true;
+    } else {
+      // webkit - safari/chrome
+      alert(
+        'Press ' +
+          (navigator.userAgent.toLowerCase().indexOf('mac') != -1
+            ? 'Command/Cmd'
+            : 'CTRL') +
+          ' + D to bookmark this page.',
+      );
+    }
+  });
+});
+
+/** @type HTMLFormElement */
+const songRequestForm = document.getElementById('SongRequestForm');
+
+if (songRequestForm) {
+  songRequestForm.addEventListener('submit', event => {
+    event.preventDefault();
+    alert('Song request received. The elders will review');
+  });
+}
