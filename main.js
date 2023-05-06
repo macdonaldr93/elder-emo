e15bf18998375a51fc8de2ecd8f3751eeb094f39
Ryan Macdonald <hi@ryanmacdonald.ca>
2023-05-06 18:55:45 -0400

    Add shitty cursor and fat lip

--- main.js
+++ main.js
@@ -1,41 +1,79 @@
 console.log('I am your elder');
 
-/** @type NodeListOf<HTMlAnchorElement> */
-const bookmarkElements = document.querySelectorAll('a[data-bookmark]');
+playAudioOnLogoClick();
+openBookmarkOnClick();
+listenForSongRequestSubmit();
 
-bookmarkElements.forEach(element => {
-  element.addEventListener('click', event => {
-    event.preventDefault();
+function playAudioOnLogoClick() {
+  const fatLipElement = document.getElementById('PlayFatLip');
+  const fatLipContentElement = document.getElementById('PlayFatLipContent');
 
-    if (window.sidebar && window.sidebar.addPanel) {
-      // Mozilla Firefox Bookmark
-      window.sidebar.addPanel(document.title, window.location.href, '');
-    } else if (window.external && 'AddFavorite' in window.external) {
-      // IE Favorite
-      window.external.AddFavorite(location.href, document.title);
-    } else if (window.opera && window.print) {
-      // Opera Hotlist
-      this.title = document.title;
-      return true;
-    } else {
-      // webkit - safari/chrome
-      alert(
-        'Press ' +
-          (navigator.userAgent.toLowerCase().indexOf('mac') != -1
-            ? 'Command/Cmd'
-            : 'CTRL') +
-          ' + D to bookmark this page.',
-      );
-    }
+  if (fatLipElement) {
+    let playing = false;
+    const audio = new Audio('fat-lip.mp3');
+
+    fatLipElement.addEventListener('click', event => {
+      event.preventDefault();
+
+      if (playing) {
+        audio.pause();
+        playing = false;
+
+        if (fatLipContentElement) {
+          fatLipContentElement.textContent = '(click me)';
+        }
+      } else {
+        audio.play();
+        playing = true;
+
+        if (fatLipContentElement) {
+          fatLipContentElement.textContent = '(click me again to pause)';
+        }
+      }
+    });
+  }
+}
+
+function openBookmarkOnClick() {
+  /** @type NodeListOf<HTMlAnchorElement> */
+  const bookmarkElements = document.querySelectorAll('a[data-bookmark]');
+
+  bookmarkElements.forEach(element => {
+    element.addEventListener('click', event => {
+      event.preventDefault();
+
+      if (window.sidebar && window.sidebar.addPanel) {
+        // Mozilla Firefox Bookmark
+        window.sidebar.addPanel(document.title, window.location.href, '');
+      } else if (window.external && 'AddFavorite' in window.external) {
+        // IE Favorite
+        window.external.AddFavorite(location.href, document.title);
+      } else if (window.opera && window.print) {
+        // Opera Hotlist
+        this.title = document.title;
+        return true;
+      } else {
+        // webkit - safari/chrome
+        alert(
+          'Press ' +
+            (navigator.userAgent.toLowerCase().indexOf('mac') != -1
+              ? 'Command/Cmd'
+              : 'CTRL') +
+            ' + D to bookmark this page.',
+        );
+      }
+    });
   });
-});
+}
 
-/** @type HTMLFormElement */
-const songRequestForm = document.getElementById('SongRequestForm');
+function listenForSongRequestSubmit() {
+  /** @type HTMLFormElement */
+  const songRequestForm = document.getElementById('SongRequestForm');
 
-if (songRequestForm) {
-  songRequestForm.addEventListener('submit', event => {
-    event.preventDefault();
-    alert('Song request received. The elders will review');
-  });
+  if (songRequestForm) {
+    songRequestForm.addEventListener('submit', event => {
+      event.preventDefault();
+      alert('Song request received. The elders will review');
+    });
+  }
 }
